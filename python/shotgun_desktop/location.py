e09928693bead497e29305d2c4741d4924fd71bd
Jean-Francois Boismenu <jean.francois.boismenu@autodesk.com>
2015-05-26 15:44:09 -0400

    addressed Rob's comments

--- python/shotgun_desktop/location.py
+++ python/shotgun_desktop/location.py
@@ -24,33 +24,45 @@ import os
 import json
 
 
-# File that gets written when this code is bundled with the desktop installer.
-# It usually points to the app_store.
+def _get_location_yaml_location(root):
+    """
+    Computes the path to the location.yml file.
 
-LOCATION_YML_LOCATION = os.path.join(
-    os.path.split(__file__)[0],
-    "..",
-    "..",
-    "location.yml"
-)
+    :param root: Path pointing to the root of a bundle.
+
+    :returns: A string pointing to root/resources/location.yml
+    """
+    return os.path.join(
+        root,
+        "resources",
+        "location.yml"
+    )
 
 
 def get_location(sgtk, app_bootstrap):
     """
     Returns a location dictionary for the bundled framework.
+
+    :param sgtk: Handle to the Toolkit API
+    :param app_bootstrap: Instance of the application bootstrap.
+
+    :returns: A dictionary with keys and values following the Toolkit location
+        convention. Read more at https://toolkit.shotgunsoftware.com/entries/23797786#Code%20Locations
     """
+    dev_descriptor = {
+        "type": "dev",
+        "path": app_bootstrap.get_startup_location_override()
+    }
     # If the startup location had been overriden, the descriptor is automatically
     # a dev descriptor.
     if app_bootstrap.get_startup_location_override():
-        return {
-            "type": "dev",
-            "path": app_bootstrap.get_startup_location_override()
-        }
+        return dev_descriptor
 
     # If we are running the bundled startup, it is possible to override it's
     # value on first run in order to trigger the system into pulling from
     # another location that the app_store, namely git. This is done through an
-    # environment variable.
+    # environment variable. This is also great for testing app_store updates
+    # by pretending you have an older version of the code bundled and need an update.
     if app_bootstrap.runs_bundled_startup() and "SGTK_DESKTOP_BUNDLED_DESCRIPTOR" in os.environ:
         json_data = json.loads(os.environ["SGTK_DESKTOP_BUNDLED_DESCRIPTOR"])
         desc = {}
@@ -72,27 +84,31 @@ def get_location(sgtk, app_bootstrap):
             desc[k] = v
         return desc
 
+    location = _get_location_yaml_location(app_bootstrap.get_startup_path())
+
+    # If the file is missing, we're in dev mode.
+    if not os.path.exists(location):
+        return dev_descriptor
+
     # Read the location.yml file. Local import since sgtk is lazily loaded.
     from tank_vendor import yaml
-    with open(LOCATION_YML_LOCATION, "r") as location_yml:
-        location_data = yaml.load(location_yml) or {}
-        return location_data.get("location")
+    with open(location, "r") as location_file:
+        # If the file is empty, we're in dev mode.
+        return yaml.load(location_file) or dev_descriptor
 
 
 def write_location(descriptor):
     """
-    Returns a location dictionary for the bundled framework.
+    Writes the descriptor dictionary to disk in the BUNDLE_ROOT/resources/location.yml file.
     """
-    # Read the location.yml file. Local import since sgtk is lazily loaded.
-    location_data = {
-        "location": descriptor.get_location()
-    }
-    # Read the location.yml file. Local import since sgtk is lazily loaded.
+    # Local import since sgtk is lazily loaded.
     from tank_vendor import yaml
     old_umask = os.umask(0077)
     try:
-        with open(os.path.join(descriptor.get_path(), "location.yml"), "w") as location_yml:
+        # Write the toolkit descriptor information to disk.
+        location_yml_path = os.path.join(_get_location_yaml_location(descriptor.get_path()))
+        with open(location_yml_path, "w") as location_yml:
             location_yml.write(copyright)
-            yaml.dump(location_data, location_yml)
+            yaml.dump(descriptor.get_location(), location_yml)
     finally:
         os.umask(old_umask)
