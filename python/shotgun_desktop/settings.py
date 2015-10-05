32eaefc7ad21bf62a9a4e28323704de8f573b65d
Jean-Francois Boismenu <jean.francois.boismenu@autodesk.com>
2015-10-05 11:27:42 -0400

    For #30835, move Desktop's config.ini ouf of the bundle config.ini is now loaded from the desktop's configuration directory inside the user folder (On Mac, that's ~/Library/Caches/Shotgun/desktop/config/config.ini). If it is not found, SGTK_DESKTOP_CONFIG_LOCATION is used. Finally, we look into the application installation folder. Closes https://github.com/shotgunsoftware/tk-framework-desktopstartup/pull/16

--- python/shotgun_desktop/settings.py
+++ python/shotgun_desktop/settings.py
@@ -39,21 +39,55 @@ class Settings(object):
     def __init__(self, bootstrap):
         """
         Constructor.
+
+        :param bootstrap: The application bootstrap.
         """
-        path = self._get_global_config_location(bootstrap)
+        path = self._get_config_location(bootstrap)
         logger.info("Reading global settings from %s" % path)
         self._global_config = self._load_config(path)
 
-    def _get_global_config_location(self, bootstrap):
+    def _get_install_dir_config_location(self, bootstrap):
         """
-        :returns: The location of the global configuration file inside the installation directory.
+        :param bootstrap: The application bootstrap.
+
+        :returns: Path to the config.ini file within the installation folder.
         """
-        if "SGTK_DEFAULT_LOGIN_DEBUG_LOCATION" in os.environ:
-            return os.environ["SGTK_DEFAULT_LOGIN_DEBUG_LOCATION"]
-        elif sys.platform == "darwin":
+        if sys.platform == "darwin":
             return os.path.join(bootstrap.get_app_root(), "Contents", "Resources", "config.ini")
         else:
             return os.path.join(bootstrap.get_app_root(), "config.ini")
+
+    def _get_user_dir_config_location(self, bootstrap):
+        """
+        :param bootstrap: The application bootstrap.
+
+        :returns: Path to the config.ini within the user folder.
+        """
+        return os.path.join(
+            bootstrap.get_shotgun_desktop_cache_location(),
+            "config",
+            "config.ini"
+        )
+
+    def _get_config_location(self, bootstrap):
+        """
+        Retrieves the location of the config.ini file. It will first look inside
+        the user folder, then look at the  SGTK_DESKTOP_CONFIG_LOCATION environment
+        variable and finally in the installation folder.
+
+        :param bootstrap: The application bootstrap.
+
+        :returns: The location where to read the configuration file from.
+        """
+        # Look inside the user folder. If it exists, return that path.
+        location = self._get_user_dir_config_location(bootstrap)
+        if os.path.exists(location):
+            return location
+
+        return os.environ.get(
+            "SGTK_DESKTOP_CONFIG_LOCATION",
+            self._get_install_dir_config_location(bootstrap)
+        )
 
     def _load_config(self, path):
         """
