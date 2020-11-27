047118e6796a7565ec539b94b10d68dea4479f9f
Ehsan Shokrgozar <ehsan.shokrgozar@autodesk.com>
2020-11-27 08:06:10 -0800

    Cleaned up the way hyperlink color is applied by moving it to a central location

--- python/shotgun_desktop/errors.py
+++ python/shotgun_desktop/errors.py
@@ -33,9 +33,6 @@ class ShotgunDesktopError(Exception):
     Common base class for Shotgun Desktop errors.
     """
 
-    # The CSS style used for hyperlinks inside error messages
-    link_style = "'color: rgb(35,165,225)'"
-
     def __init__(self, message, support_required=False):
         """
         :param message: Error message to display.
@@ -44,13 +41,13 @@ class ShotgunDesktopError(Exception):
         """
 
         if support_required:
-            support_message = "Please <a style={} href={}>contact support</a> to resolve this issue.".format(
-                self.link_style, sgtk.support_url
+            support_message = "Please <a href={}>contact support</a> to resolve this issue.".format(
+                sgtk.support_url
             )
         else:
             support_message = (
-                "If you need help with this issue, please <a style={} href={}>contact support</a>."
-            ).format(self.link_style, sgtk.support_url)
+                "If you need help with this issue, please <a href={}>contact support</a>."
+            ).format(sgtk.support_url)
         Exception.__init__(self, "%s\n\n%s" % (message, support_message))
 
 
@@ -142,9 +139,8 @@ class EngineNotCompatibleWithDesktop16(ShotgunDesktopError):
             "Your version of tk-desktop is not compatible with Shotgun Desktop {}.\n"
             "\n"
             "Please upgrade your site configuration's tk-desktop to v2.5.9+ or "
-            "download Shotgun Desktop 1.5.9 or earlier <a style={} href='{}'>here</a>".format(
+            "download Shotgun Desktop 1.5.9 or earlier <a href='{}'>here</a>".format(
                 app_version,
-                self.link_style,
                 "https://support.shotgunsoftware.com/hc/en-us/articles/219039888-Shotgun-Desktop-Release-Notes",
             )
         )
