06d787fe64e88a17352a89f2e090eae68584b3ae
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2023-04-26 09:30:08 -0500

    Add print messages to updater script.
    Support sed command for macOS implementation

--- tk_core_update.sh
+++ tk_core_update.sh
@@ -10,6 +10,20 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+echo "This script will update the local repository with the given version of the TK-CORE."
+echo ""
+echo "Script syntax: $0 tk-core-tag"
+echo "For example:   $0 v0.20.17"
+echo ""
+echo ""
+echo "A commit_id file will be written to the tk-core shotgun distribution to "
+echo "indicate which version being bundled with core."
+echo ""
+echo "This script is intended to be used by developers and maintainers of the tk-core API."
+echo ""
+echo ""
+echo ""
+
 # Stops the script
 set -e
 
@@ -43,8 +57,12 @@ mkdir $DEST_REPO
 # Strip files from the destination and recreate it.
 rm -rf $DEST
 mkdir -p $DEST
+
+echo "Cloning TK-CORE into a temp location, hang on..."
 # Clone the repo
 git clone $SRC_REPO $DEST_REPO --depth=1 -b $1
+
+echo "Copying TK-CORE to the required location..."
 
 # Move to the git repo to generate the sha and write it to the $DEST
 pushd $DEST_REPO
@@ -56,14 +74,19 @@ cp -R $DEST_REPO/* $DEST
 rm -rf $DEST/tests
 rm -rf $DEST/docs
 
-sed -i $DEST/info.yml -e "s/version: \"HEAD\"/version: \"$1\"/"
+echo "Updating tk-core info.yml..."
+sed -i "" -e "s/version: \"HEAD\"/version: \"$1\"/" $DEST/info.yml
 
 cp python/tk-core/software_credits software_credits
 git add software_credits
 
 # Put files in the staging area.
+echo "adding new files to git..."
 git add -A $DEST
 # Cleanup!
 rm -rf $ROOT
 
+echo ""
+echo "All done! TK-CORE in $DEST has been updated to $1."
+echo "The changes have been added to git and are ready to be committed."
 trap : 0
