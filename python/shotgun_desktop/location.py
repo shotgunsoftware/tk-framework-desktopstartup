688c88cc31656c48613580afb7c304dc25ee4ad1
Norberto Moreno Velez <norbermv.dev@gmail.com>
2023-08-30 20:48:47 -0500

    Remove unused py2 method.

--- python/shotgun_desktop/location.py
+++ python/shotgun_desktop/location.py
@@ -80,30 +80,9 @@ def get_startup_descriptor(sgtk, sg, app_bootstrap):
     # Use the old API to create the descriptor, as we might be using a 0.16-based core.
     return sgtk.deploy.descriptor.get_from_location_and_paths(
         sgtk.deploy.descriptor.AppDescriptor.FRAMEWORK,
-        app_bootstrap.get_shotgun_desktop_cache_location(),  # ~/Library/Caches/Shotgun/desktop
+        app_bootstrap.get_shotgun_desktop_cache_location(),  
         os.path.join(app_bootstrap.get_shotgun_desktop_cache_location(), "install"),
         get_location(app_bootstrap),
-    )
-
-
-def get_latest_py2_descriptor(sgtk, sg, app_bootstrap):
-    """
-    Creates a startup descriptor based on the current desktop startup supported
-    Python 2 version.
-
-    :returns: :class:`sgtk.descriptor.FrameworkDescriptor` instance.
-    """
-    startup_py2_version = {
-        "type": "app_store",
-        "name": "tk-framework-desktopstartup",
-        "version": "v2.1.12",
-    }
-    # Use the old API to create the descriptor, as we might be using a 0.16-based core.
-    return sgtk.deploy.descriptor.get_from_location_and_paths(
-        sgtk.deploy.descriptor.AppDescriptor.FRAMEWORK,
-        app_bootstrap.get_shotgun_desktop_cache_location(),  # ~/Library/Caches/Shotgun/desktop
-        os.path.join(app_bootstrap.get_shotgun_desktop_cache_location(), "install"),
-        startup_py2_version,
     )
 
 
