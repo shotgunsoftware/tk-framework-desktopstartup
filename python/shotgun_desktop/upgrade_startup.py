849ed9039fb80cc14d77696d4b99ae7eac39712f
Norberto Moreno Velez <norbermv.dev@gmail.com>
2023-08-31 10:08:15 -0500

    Update docstring and comments.

--- python/shotgun_desktop/upgrade_startup.py
+++ python/shotgun_desktop/upgrade_startup.py
@@ -33,10 +33,22 @@ def _supports_get_from_location_and_paths(sgtk):
 
 
 def _out_of_date_check(latest_descriptor, current_desc):
-    """Check if the version is out of date."""
-    location = current_desc.get_path()
-    # Check if we're running in Python 2
-    if sys.version_info[0] < 3 and os.path.exists(location):
+    """
+    Check if the version is out of date, this prevents an upgrade of the startup logic
+    in the event that it detects SG Desktop is running on Python 2.
+
+    :param latest_descriptor:`sgtk.descriptor.FrameworkDescriptor` instance with the latest startup descriptor.
+    :param current_desc:`sgtk.descriptor.FrameworkDescriptor` instance with the current startup descriptor.
+
+    :returns: True if the startup version is outdated  update was downloaded and the descriptor updated, False otherwise.
+    """
+
+    # If we're running in Python 2 and if the bundled framework exists on disk,
+    # returns False to avoid upgrade the startup logic.
+    if sys.version_info[0] < 3 and os.path.exists(current_desc.get_path()):
+        logger.debug(
+            "Using Python version '%s'" % ".".join(str(i) for i in sys.version_info[0:3])
+        )
         logger.debug(
             "Desktop startup is Currently running version %s"
             % current_desc.get_version(),
